f8e82f7d2a1ff81657bedbbca9217361bfde1639
杨楠 <yangnan@nihaosi.com>
2022-02-17 17:30:00 +0800

    fix:新增用户管理页面 #1

---
+++ src/pages/UserManagement/User/Search/index.tsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { Dispatch } from '@uiw-admin/models'
+import { ProTable, useTable } from '@uiw-admin/components'
+import { Usermanagement } from '@/servers/usermanagement'
+import { item } from './item'
+import { FormCol } from '@uiw-admin/components/lib/ProTable/types'
+import Drawer from '../Drawers'
+import Modals from '../Modals'
+
+interface State {
+  drawerVisible?: boolean
+  tableType?: string
+  queryInfo?: object
+  isView?: boolean
+  delectVisible?: boolean
+  systemId?: string
+}
+export default function Search() {
+  const dispatch = useDispatch<Dispatch>()
+
+  const updateData = (payload: State) => {
+    dispatch({
+      type: 'usermanagement/updateState',
+      payload,
+    })
+  }
+
+  const table = useTable('/api/account/selectPage', {
+    // 格式化接口返回的数据，必须返回{total 总数, data: 列表数据}的格式
+    formatData: (data) => {
+      return {
+        total: data?.data?.total,
+        data: data?.data?.rows || [],
+      }
+    },
+    // 格式化查询参数 会接收到pageIndex 当前页  pageSize 页码
+    query: (pageIndex, searchValues) => {
+      function timestampToTime(timestamp: string) {
+        let d = new Date(timestamp)
+        let month =
+          d.getMonth() > 8 ? d.getMonth() + 1 : '0' + (d.getMonth() + 1)
+        let date = d.getFullYear() + '-' + month + '-' + d.getDate()
+        let hour = d.getHours() > 9 ? d.getHours() : '0' + d.getHours()
+        let minute = d.getMinutes() > 9 ? d.getMinutes() : '0' + d.getMinutes()
+        let ss = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds()
+        let time = date + ' ' + hour + ':' + minute + ':' + ss
+        return time
+      }
+      return {
+        page: pageIndex,
+        pageSize: 10,
+        realName: searchValues.uapUserRealName,
+        uapUserMobile: searchValues.uapUserMobile,
+        beginTime: searchValues.beginTime
+          ? timestampToTime(searchValues.beginTime)
+          : '',
+        endTime: searchValues.endTime
+          ? timestampToTime(searchValues.endTime)
+          : '',
+      }
+    },
+  })
+  // 操作
+  function handleEditTable(type: string, obj: Usermanagement) {
+    updateData({
+      isView: type === 'view',
+      tableType: type,
+    })
+    if (type === 'edit') {
+      updateData({ drawerVisible: true, queryInfo: obj })
+    }
+    if (type === 'del') {
+      updateData({ delectVisible: true, systemId: obj?.id })
+    }
+  }
+
+  return (
+    <React.Fragment>
+      <ProTable
+        bordered
+        searchBtns={[
+          {
+            label: '查询',
+            type: 'primary',
+            htmlType: 'submit',
+            onClick: () => {
+              table.onSearch()
+            },
+          },
+          {
+            label: '重置',
+            onClick: () => {
+              updateData({ queryInfo: {} })
+              table.onReset()
+            },
+          },
+        ]}
+        columns={item(handleEditTable) as FormCol[]}
+        table={table}
+      />
+      <Drawer updateData={updateData} onSearch={table.onSearch} />
+      <Modals onSearch={table.onSearch} />
+    </React.Fragment>
+  )
+}
---
+++ src/pages/UserManagement/User/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Search from './Search'
+export default function Usermanagement() {
+  return (
+    <React.Fragment>
+      <Search />
+    </React.Fragment>
+  )
+}
