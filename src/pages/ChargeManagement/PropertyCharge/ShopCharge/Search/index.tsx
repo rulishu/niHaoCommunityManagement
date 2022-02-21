b4f454690b89fcb2e3111516fc4ed998065b1cab
杨楠 <yangnan@nihaosi.com>
2022-02-21 18:24:27 +0800

    fix:商铺综合收费页面优化 #15

--- src/pages/ChargeManagement/PropertyCharge/ShopCharge/Search/index.tsx
+++ src/pages/ChargeManagement/PropertyCharge/ShopCharge/Search/index.tsx
@@ -1,68 +1,100 @@
-import React, { useEffect } from 'react'
-import { useTable } from '@uiw-admin/components'
-import { Params } from '@/servers/account-admin'
-import { ProForm, useForm } from '@uiw-admin/components'
-// import { columnsSearch } from './item';
-// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { Form, Row, Col, SearchSelect, Button } from 'uiw'
 import { Card } from 'uiw'
+import Table from '../Table/index'
+import { useDispatch } from 'react-redux'
+import { Dispatch } from '@uiw-admin/models'
+import './index.css'
 
-export default function Demo(props: {
-  setVisible: React.Dispatch<React.SetStateAction<boolean>>
-  setTableType: React.Dispatch<React.SetStateAction<'add' | 'edit' | null>>
-  visible: boolean
-  tableType: 'add' | 'edit' | null
-  setInitObj: React.Dispatch<React.SetStateAction<Params>>
-}) {
-  const form = useForm()
+interface State {
+  drawerVisible?: boolean
+  tableType?: string
+  queryInfo?: object
+  isView?: boolean
+  delectVisible?: boolean
+  id?: string
+}
 
-  const search = useTable('后端接口', {
-    // 格式化接口返回的数据，必须返回{total 总数, data: 列表数据}的格式
-    formatData: (data) => {
-      return {
-        total: data?.data?.total,
-        data: data?.data?.row || [],
-      }
-    },
-    // 格式化查询参数 会接收到pageIndex 当前页  searchValues 表单数据
-    query: (pageIndex, searchValues) => {
-      return {
-        page: pageIndex,
-        pageSize: 10,
-        data: searchValues,
-      }
-    },
-  })
-  useEffect(() => {
-    props.tableType && !props.visible && search.onSearch()
-  }, [props.visible])
+const selectOption = [
+  { label: 'a', value: 2 },
+  { label: 'aa', value: 3 },
+  { label: 'aaa', value: 4 },
+]
+
+export default function Demo() {
+  const dispatch = useDispatch<Dispatch>()
+
+  const updateData = (payload: State) => {
+    dispatch({
+      type: 'ShopCharge/updateState',
+      payload,
+    })
+  }
+
+  const [option] = React.useState(selectOption)
+  const [loading, setLoading] = React.useState(false)
+  const handleSearch = () => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }
 
   return (
     <Card>
-      <ProForm
-        showSaveButton
-        saveButtonProps={{
-          label: '搜索',
-          type: 'primary',
+      <Form
+        onSubmit={({ initial, current }) => {
+          console.log('-->>', initial, current)
+          updateData({ drawerVisible: true, queryInfo: current })
         }}
-        onSubmit={(initial, current) => {
-          console.log('current', current, 'initial', initial)
-          // 调用请求接口
-        }}
-        form={form}
-        formType="pure"
-        formDatas={[
-          {
+        fields={{
+          selectField: {
+            labelClassName: 'fieldLabel',
+            labelStyle: { width: '100%' },
             label: '商铺编号',
-            key: 'input',
-            widget: 'input',
-            initialValue: '',
-            widgetProps: {},
-            span: '24',
-            // rules: [
-            // ]
+            inline: true,
+            children: (
+              <SearchSelect
+                showSearch={true}
+                allowClear
+                disabled={false}
+                placeholder="请选择商铺编号"
+                onSearch={handleSearch}
+                onChange={(v) => {
+                  console.log('onChange', v)
+                }}
+                option={option}
+                loading={loading}
+              />
+            ),
           },
-        ]}
-      />
+        }}
+      >
+        {({ fields, state, canSubmit }) => {
+          console.log('fields:', state)
+          return (
+            <div className="uiw-form">
+              <Row gutter={10} style={{ paddingTop: 10 }}>
+                <Col>{fields.selectField}</Col>
+              </Row>
+              <Row gutter={10}>
+                <Col />
+                <Col fixed align="bottom">
+                  <Button
+                    disabled={!canSubmit()}
+                    type="primary"
+                    htmlType="submit"
+                  >
+                    提交
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+          )
+        }}
+      </Form>
+
+      <Table updateData={updateData} />
     </Card>
   )
 }
--- src/pages/ChargeManagement/PropertyCharge/ShopCharge/Table/index.tsx
+++ src/pages/ChargeManagement/PropertyCharge/ShopCharge/Table/index.tsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import Regular from './Regular';
-// import Temporary from './temporary';
-// import Deposit from './deposit';
-// import Advance from './advance'
-import { Tabs, Card } from 'uiw';
-import { Params } from '@/servers/account-admin';
+import React from 'react'
+import { Tabs, Card } from 'uiw'
+import { useSelector } from 'react-redux'
+import { RootState } from '@uiw-admin/models'
+import Regular from './Regular'
 
-export default function Demo(props: {
-    setVisible: React.Dispatch<React.SetStateAction<boolean>>;
-    setTableType: React.Dispatch<React.SetStateAction<'add' | 'edit' | null>>;
-    setKeys: React.Dispatch<React.SetStateAction<string>>
-    visible: boolean;
-    tableType: 'add' | 'edit' | null;
-    initObj: Params;
-    keys: string
-}) {
-    return (
-        <Card style={{ marginTop: 10 }}>
-            <Tabs type="line" activeKey="1" onTabClick={(tab) => {
-                props.setVisible(true);
-                props.setTableType('add');
-                props.setKeys(tab);
-            }}>
-                <Tabs.Pane label="常规收费" key="1">
-                    <Regular keys={props.keys} />
-                </Tabs.Pane>
-                <Tabs.Pane label="临时收费" key="2">
-                    <Regular keys={props.keys} />
-                </Tabs.Pane>
-                <Tabs.Pane label="收取押金" key="3">
-                    <Regular keys={props.keys} />
-                </Tabs.Pane>
-                <Tabs.Pane label="预存款" key="4">
-                    <Regular keys={props.keys} />
-                </Tabs.Pane>
-            </Tabs>
-        </Card>
-    )
+interface State {
+  drawerVisible?: boolean
+  tableType?: string
+  queryInfo?: object
+  isView?: boolean
+  keys?: string
 }
 
+const Demo = (props: { updateData: (payload: State) => void }) => {
+  const {
+    ShopCharge: { keys },
+  } = useSelector((ShopCharge: RootState) => ShopCharge)
+
+  return (
+    <Card style={{ marginTop: 10 }}>
+      <Tabs
+        type="line"
+        activeKey="1"
+        onTabClick={(tab) => {
+          props.updateData({ keys: tab })
+        }}
+      >
+        <Tabs.Pane label="常规收费" key="1">
+          <Regular keys={keys} />
+        </Tabs.Pane>
+        <Tabs.Pane label="临时收费" key="2">
+          <Regular keys={keys} />
+        </Tabs.Pane>
+        <Tabs.Pane label="收取押金" key="3">
+          <Regular keys={keys} />
+        </Tabs.Pane>
+        <Tabs.Pane label="预存款" key="4">
+          <Regular keys={keys} />
+        </Tabs.Pane>
+      </Tabs>
+    </Card>
+  )
+}
+
+export default Demo
