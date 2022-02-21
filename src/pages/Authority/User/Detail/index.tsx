ab8390fbea1bd285469303baf96bf6f380f01460
杨楠 <yangnan@nihaosi.com>
2022-02-21 13:52:46 +0800

    fix:新增用户权限管理页面 #12

---
+++ src/pages/Authority/User/Detail/index.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { ProDrawer, ProForm, useForm } from '@uiw-admin/components'
+import { Notify } from 'uiw'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState, Dispatch } from '@uiw-admin/models'
+import { insert, update } from '@/servers/Authority/User'
+import { items } from './items'
+import useSWR from 'swr'
+
+interface State {
+  drawerVisible?: boolean
+  tableType?: string
+  queryInfo?: object
+  isView?: boolean
+}
+
+const Detail = (props: {
+  updateData: (payload: State) => void
+  onSearch: () => void
+}) => {
+  const baseRef = useForm()
+  const dispatch = useDispatch<Dispatch>()
+  const {
+    User: { drawerVisible, tableType, queryInfo, isView },
+  } = useSelector((User: RootState) => User)
+
+  const onClose = () => {
+    dispatch({
+      type: 'User/updateState',
+      payload: {
+        drawerVisible: false,
+        isView: false,
+      },
+    })
+  }
+
+  const { mutate } = useSWR(
+    [
+      (tableType === 'add' && insert) || (tableType === 'edit' && update),
+      { method: 'POST', body: queryInfo },
+    ],
+    {
+      revalidateOnMount: false,
+      revalidateOnFocus: false,
+      onSuccess: (data) => {
+        if (data && data.code === 1) {
+          Notify.success({ title: data.message })
+          onClose()
+          props.onSearch()
+        } else {
+          Notify.error({ title: '提交失败！' })
+        }
+      },
+    }
+  )
+
+  return (
+    <ProDrawer
+      width={800}
+      title={
+        tableType === 'add' ? '新增' : tableType === 'edit' ? '编辑' : '查看'
+      }
+      visible={drawerVisible}
+      onClose={onClose}
+      buttons={[
+        {
+          label: '取消',
+          onClick: onClose,
+          show: !isView,
+        },
+        {
+          label: '保存',
+          type: 'primary',
+          style: { textAlign: 'right' },
+          show: !isView,
+          onClick: async () => {
+            await baseRef?.submitvalidate?.()
+            const errors = baseRef.getError()
+            if (errors && Object.keys(errors).length > 0) return
+            mutate()
+          },
+        },
+      ]}
+    >
+      <ProForm
+        title="基础信息"
+        formType={isView ? 'pure' : 'card'}
+        form={baseRef}
+        readOnly={isView}
+        buttonsContainer={{ justifyContent: 'flex-start' }}
+        // 更新表单的值
+        onChange={(initial, current) =>
+          props.updateData({ queryInfo: { ...queryInfo, ...current } })
+        }
+        formDatas={items(queryInfo)}
+      />
+    </ProDrawer>
+  )
+}
+
+export default Detail
