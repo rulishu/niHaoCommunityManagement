17b3d7c59f54223413133acfa16d6f279140e932
杨楠 <yangnan@nihaosi.com>
2022-03-02 16:24:59 +0800

    fix:用户管理页面接口调用 #36 #39

--- src/pages/UserManagement/User/Drawers/items.tsx
+++ src/pages/UserManagement/User/Drawers/items.tsx
@@ -1,32 +1,70 @@
 import { Usermanagement } from '@/servers/usermanagement'
 
-export const items = (queryInfo: Usermanagement) => [
+export const items = (queryInfo: Usermanagement, isView: boolean) => [
   {
     label: '账号名称',
-    key: 'uapAccountNickName',
+    key: 'createName',
     widget: 'input',
-    initialValue: queryInfo?.uapAccountNickName,
-    required: true,
-    rules: [{ required: true, message: '请输入账号名称' }],
-    widgetProps: {},
-    placeholder: '请输入账号名称',
+    initialValue: queryInfo?.createName,
+    hide: !isView,
   },
   {
-    label: '用户真实姓名',
-    key: 'uapUserRealName',
+    label: '用户姓名',
+    key: 'userName',
     widget: 'input',
-    initialValue: queryInfo?.uapUserRealName,
+    initialValue: queryInfo?.userName,
     required: true,
     rules: [{ required: true, message: '请输入用户真实姓名' }],
     placeholder: '请输入用户真实姓名',
   },
   {
-    label: '角色名称',
-    key: 'uapRoleName',
+    label: '联系方式',
+    key: 'phoneNumber',
     widget: 'input',
-    initialValue: queryInfo?.uapRoleName,
+    initialValue: queryInfo?.phoneNumber,
     required: true,
-    rules: [{ required: true, message: '请输入角色名称' }],
     placeholder: '请输入角色名称',
+    rules: [
+      {
+        pattern: new RegExp(
+          /^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$/
+        ),
+        message: '请正确输入手机号',
+      },
+    ],
+  },
+  {
+    label: '用户性别',
+    key: 'gender',
+    widget: 'input',
+    initialValue: queryInfo?.gender,
+    hide: !isView,
+  },
+  {
+    label: '用户状态',
+    key: 'status',
+    widget: 'select',
+    option: [
+      { value: 1, label: '正常' },
+      { value: 2, label: '停用' },
+    ],
+    required: true,
+    rules: [{ required: true, message: '请选择用户状态' }],
+    placeholder: '请选择用户状态',
+    initialValue: queryInfo?.status,
+  },
+  {
+    label: '注册时间',
+    key: 'createTime',
+    widget: 'input',
+    initialValue: queryInfo?.createTime,
+    hide: !isView,
+  },
+  {
+    label: '备注',
+    key: 'remark',
+    widget: 'input',
+    initialValue: queryInfo?.remark,
+    placeholder: '请输入备注',
   },
 ]
