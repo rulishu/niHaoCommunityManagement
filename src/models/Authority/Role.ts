1379e98a9bd467885773d63d5a6c2015e92eb9dd
杨楠 <yangnan@nihaosi.com>
2022-03-04 11:39:40 +0800

    fix:增加授权功能,角色管理-配置菜单接口联及角色权限设置页面相关接口调用 #40 #95

--- src/models/Authority/Role.ts
+++ src/models/Authority/Role.ts
@@ -1,6 +1,7 @@
 import { selectById, Change } from '../../servers/Authority/Role'
 import { Dispatch, RootModel } from '@uiw-admin/models'
 import { createModel, RematchDispatch } from '@rematch/core'
+import { TreeData } from '@uiw/react-tree'
 
 interface State {
   drawerVisible: boolean
@@ -9,6 +10,8 @@ interface State {
   isView: boolean
   id: string
   delectVisible: boolean
+  menuList: TreeData[]
+  selectMenu: TreeData['key'][]
 }
 
 const Role = createModel<RootModel>()({
@@ -20,6 +23,8 @@ const Role = createModel<RootModel>()({
     id: '',
     isView: false,
     delectVisible: false,
+    menuList: [], //菜单列表
+    selectMenu: [], //已选中菜单列表
   } as State,
   reducers: {
     updateState: (state: State, payload: Partial<State>) => ({
@@ -32,9 +37,40 @@ const Role = createModel<RootModel>()({
       const dph = dispatch as Dispatch
       const data = await selectById(payload)
       if (data.code === 1) {
-        dph.users.updateState({
-          drawerVisible: true,
-          queryInfo: data.data || {},
+        let arr = data.data.menuList // 菜单数据
+
+        arr.map((item: any) => {
+          // 根级菜单数据处理
+          item.label = item.menuName
+          item.key = item.id
+          if (item.children.length === 0) {
+            delete item.children
+          }
+          // 一级菜单数据处理
+          if (item.children && item.children.length > 0) {
+            item.children[0].label = item.children[0].menuName
+            item.children[0].key = item.children[0].id
+            if (item.children[0].children.length === 0) {
+              delete item.children[0].children
+            }
+            // 二级菜单数据处理
+            if (
+              item.children[0].children &&
+              item.children[0].children.length > 0
+            ) {
+              item.children[0].children.map((itm: any) => {
+                itm.label = itm.menuName
+                itm.key = itm.id
+                if (itm.children.length === 0) {
+                  delete itm.children
+                }
+              })
+            }
+          }
+        })
+        dph.Role.updateState({
+          menuList: arr,
+          selectMenu: data.data.selected,
         })
       }
     },
--- src/pages/Authority/Role/Detail/items.tsx
+++ src/pages/Authority/Role/Detail/items.tsx
@@ -1,6 +1,6 @@
 import { Change } from '@/servers/Authority/Role'
 
-export const items = (queryInfo: Change) => [
+export const itemsAdd = (queryInfo: Change, isView: boolean) => [
   {
     label: '角色名称',
     key: 'roleName',
@@ -8,25 +8,43 @@ export const items = (queryInfo: Change) => [
     initialValue: queryInfo?.roleName,
     required: true,
     rules: [{ required: true, message: '请输入角色名称' }],
+    span: '24',
   },
-  // {
-  //   label: '角色状态',
-  //   key: 'status',
-  //   widget: 'select',
-  //   option: [
-  //     { label: '正常', value: 1 },
-  //     { label: '停用', value: 2 },
-  //   ],
-  //   initialValue: queryInfo?.status,
-  //   required: true,
-  //   rules: [{ required: true, message: '请选择角色状态' }],
-  // },
-  // {
-  //   label: '备注',
-  //   key: 'remark',
-  //   widget: 'textarea',
-  //   initialValue: queryInfo?.remark,
-  //   required: true,
-  //   rules: [{ required: true, message: '请输入备注' }],
-  // },
+  {
+    label: '角色状态',
+    key: 'status',
+    widget: 'input',
+    initialValue: queryInfo?.status,
+    hide: !isView,
+  },
+  {
+    label: '创建人',
+    key: 'createName',
+    widget: 'input',
+    initialValue: queryInfo?.createName,
+    hide: !isView,
+  },
+  {
+    label: '创建时间',
+    key: 'createTime',
+    widget: 'input',
+    initialValue: queryInfo?.createTime,
+    hide: !isView,
+  },
+  {
+    label: '更新时间',
+    key: 'updateTime',
+    widget: 'input',
+    initialValue: queryInfo?.updateTime,
+    hide: !isView,
+  },
+  {
+    label: '备注',
+    initialValue: queryInfo?.remark,
+    key: 'remark',
+    placeholder: '请输入备注',
+    widget: 'textarea',
+    widgetProps: {},
+    span: '24',
+  },
 ]
