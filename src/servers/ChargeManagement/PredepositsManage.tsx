bd41ced84971c0176f9759e12c5e070102588f35
rulishu <rulishu@jz-sh.cn>
2022-03-21 16:15:17 +0800

    fix: 预存款管理页面 #126

---
+++ src/servers/ChargeManagement/PredepositsManage.tsx
@@ -0,0 +1,38 @@
+import { request } from '@uiw-admin/utils'
+
+export interface Change {
+  id?: string
+  code?: string
+  name?: string
+  project?: string
+  paymentMethod?: string
+  price?: number
+  collectionName?: string
+  collectionTime?: string
+  status?: string
+  remark?: string
+  customerType?: string
+  refundTime?: string
+  refundMethod?: string
+}
+
+/**
+ * 停车场管理-停车场管理-查询
+ * @param {Object} params
+ */
+function selectById(params: Change) {
+  return request('/api/test/select', {
+    method: 'POST',
+    body: { ...params },
+  })
+}
+
+const selectPage = '/api/test/select'
+
+const update = '/api/test/select'
+
+const insert = '/api/test/select'
+
+const deleteData = '/api/test/select'
+
+export { selectById, selectPage, update, insert, deleteData }
