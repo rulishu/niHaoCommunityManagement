18f6353c809c5ca8933e520b8ceeeab97a892fdf
rulishu <rulishu@jz-sh.cn>
2022-03-21 10:19:55 +0800

    fix: 商铺租售无值报错问题 #125

--- src/pages/BasicManage/ShopSale/Detail/index.tsx
+++ src/pages/BasicManage/ShopSale/Detail/index.tsx
@@ -65,7 +65,7 @@ const Detail = (props: {
   const deatailTable = useTable(contactSelectPage, {
     // 格式化接口返回的数据，必须返回{total 总数, data: 列表数据}的格式
     formatData: (data) => {
-      let buCharge = data?.data.map((item: any) => {
+      let buCharge = data?.data?.map((item: any) => {
         return item.buCharge
       })
       return {
