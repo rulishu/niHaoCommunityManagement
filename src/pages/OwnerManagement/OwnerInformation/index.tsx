1adf05f1c6e0b6e3ad833c3aeef716a37ff3b7ce
左嘉辉 <zuojiahui@zhouzhoudeMac.local>
2022-04-18 15:38:03 +0800

    feat: 添加业主管理页面，调用查询接口

---
+++ src/pages/OwnerManagement/OwnerInformation/index.tsx
@@ -0,0 +1,122 @@
+import { Fragment } from 'react'
+import { ProTable, useTable } from '@uiw-admin/components'
+import { Icon } from 'uiw'
+export default function Index() {
+  // 查询
+  const table = useTable('/api/buOwnerManage/selectInfo', {
+    query: (pageIndex, pageSize, searchValues) => {
+      return {
+        page: pageIndex,
+        pageSize: 10,
+        ...searchValues,
+      }
+    },
+
+    formatData: (data) => {
+      return {
+        total: data?.data?.total || 0,
+        data: data?.data?.rows || [],
+      }
+    },
+  })
+  return (
+    <Fragment>
+      <div className="proTableBox">
+        <ProTable
+          table={table}
+          searchBtns={[
+            {
+              label: '搜索',
+              type: 'primary',
+              htmlType: 'submit',
+              onClick: () => table?.onSearch(),
+              icon: <Icon type="search" style={{ fontSize: 14 }} />,
+            },
+            {
+              label: '重置',
+              onClick: () => table?.onReset(),
+              icon: <Icon type="reload" style={{ fontSize: 14 }} />,
+            },
+          ]}
+          columns={[
+            {
+              title: '姓名',
+              key: 'userName',
+              width: 180,
+              align: 'center',
+              ellipsis: true,
+              props: {
+                widget: 'input',
+                widgetProps: {
+                  placeholder: '请输入姓名',
+                },
+              },
+            },
+            {
+              title: '身份证号',
+              key: 'cardId',
+              width: 180,
+              align: 'center',
+              ellipsis: true,
+              props: {
+                widget: 'input',
+                widgetProps: {
+                  placeholder: '请输入身份证号',
+                },
+              },
+            },
+            {
+              title: '手机号',
+              key: 'phoneNumber',
+              width: 180,
+              align: 'center',
+              ellipsis: true,
+              props: {
+                widget: 'input',
+                widgetProps: {
+                  placeholder: '请输入手机号',
+                },
+              },
+            },
+            {
+              title: '性别',
+              key: 'gender',
+              width: 180,
+              align: 'center',
+              ellipsis: true,
+              render: (text: any) => (
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  {text === 1 ? '男' : text === 2 ? '女' : ''}
+                </div>
+              ),
+            },
+            {
+              title: '账号状态',
+              key: 'status',
+              width: 180,
+              align: 'center',
+              ellipsis: true,
+              render: (text: any) => (
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  {text === 1 ? '正常' : text === 2 ? '停用' : ''}
+                </div>
+              ),
+            },
+          ]}
+        />
+      </div>
+    </Fragment>
+  )
+}
