3a6e0b9c68ff66c7a054752c57cffb51f55ac3f4
左嘉辉 <zuojiahui@zhouzhoudeMac.local>
2022-04-19 16:01:42 +0800

    fix: 删除重复页面

--- src/pages/ChargeManagement/ChargeData/ShopChargeData/index.tsx
+++ src/pages/ChargeManagement/ChargeData/ShopChargeData/index.tsx
@@ -1,12 +1,5 @@
-import React from 'react'
-import Search from './Search'
-
 const Index = () => {
-  return (
-    <React.Fragment>
-      <Search />
-    </React.Fragment>
-  )
+  return <></>
 }
 
 export default Index
