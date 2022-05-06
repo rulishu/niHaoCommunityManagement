f2b4f9ac12fe389f56dfbbd2647a14ee72047a12
宋玉 <song@songyudeMacBook-Air.local>
2022-05-06 09:51:47 +0800

    feat: 【首页】替换首页统计结果API #207

--- src/pages/Home/Rows/index.tsx
+++ src/pages/Home/Rows/index.tsx
@@ -1,12 +1,26 @@
 import { Row, Col, Icon, Card } from 'uiw'
 import './index.css'
+import useSWR from "swr";
+import { request } from "@uiw-admin/utils";
 
-export default function Rows() {
+export default function Rows() {  
+  const { data } = useSWR(
+  [
+    "api/home/statistical",
+    {
+      method: "POST",
+    },
+  ],
+  request
+);
   const list = [
-    { id: 0, num: 26, title: '待收费数据（条）' },
-    { id: 1, num: 27, title: '待收费金额（元）' },
-    { id: 2, num: 25, title: '欠费数据（条）' },
-    { id: 3, num: 27, title: '欠费金额（元）' },
+    { id: 0, num: data?.data?.unchargedData|| 0, title: '待收费数据(条)' },
+    { id: 1, num: data?.data?.unchargedMoney|| 0, title: '待收费金额(元)' },
+    { id: 2, num: data?.data?.lackData|| 0, title: '欠费数据(条)' },
+    { id: 3, num: data?.data?.lackMoney|| 0, title: '欠费金额(元)' },
+    { id: 4, num: data?.data?.rentQuantity|| 0, title: '商铺出租数' },
+    { id: 5, num: data?.data?.sellQuantity|| 0, title: '商铺出售数' },
+    { id: 6, num: data?.data?.vacancyQuantity|| 0, title: '商铺空置数' },
   ]
 
   function randomColor() {
@@ -15,6 +29,7 @@ export default function Rows() {
       ('00000' + ((Math.random() * 16777215 + 0.5) >> 0).toString(16)).slice(-6)
     )
   }
+  console.log('randomColor()',randomColor())
   return (
     <Row>
       {list.map((item, index) => {
